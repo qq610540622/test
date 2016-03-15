2811ea363595ba940a983af298c7e11e092ecbd7
qq610540622 <610540622@qq.com>
2016-03-15 18:12:49 +0800

    test

---
+++ app.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+
+var app = express();
+
+
+app.set('views',['./views/show','./views/admin']);
+app.set('view engine', 'html');
+app.engine('html', require('ejs-mate'));
+//app.locals._layoutFile = 'layout.html';
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get("/",function(req,res) {
+  res.render("index");
+})
+
+app.get("/admin",function(req,res) {
+  res.render("home");
+})
+
+
+
+/*
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+*/
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("server listen to port:%s",port);
+
